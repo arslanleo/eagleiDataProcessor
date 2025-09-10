26fffb38842f651fbe57e8dc365904f972a8d8dc
wert565 <charlotte.wertz@wsu.edu>
2025-09-10 11:38:34 -0700

    processed counties weather+outage

--- map_outage+weather.py
+++ map_outage+weather.py
@@ -3,13 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-state="Illinois"
-county='Cook'
+state="Texas"
+county='Harris'
 start='2018'
 end='2024'
 # Load MCC Data
 pdf = pd.read_csv('MCC.csv')
-target_fips = '17031'  # For Cook County
+# target_fips = '17031'  # For Cook County
+#target_fips = '4013' # maricopa
+#target_fips = '25025' # suffolk
+#target_fips = '12086' # miami-dade
+#target_fips = '53033' # king
+target_fips = '48201' # harris
 result = pdf[pdf['County_FIPS'] == target_fips]
 customers = result['Customers'].values
 print(customers)
